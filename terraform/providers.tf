237cf2de560bce4d8ea7cfa2490b93e67d844f9a
hauteha <haunui.tehahe@ynov.com>
2025-11-28 14:49:01 +0100

    maj

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "registry.terraform.io/hashicorp/aws"
-      version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
