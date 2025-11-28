7f6e3adc607ab0a40d579aea6f774e0450122e0c
hauteha <haunui.tehahe@ynov.com>
2025-11-28 14:44:20 +0100

    maj

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_version = ">= 1.0.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
