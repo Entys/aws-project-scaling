01c642d9f7de61e46a5229c6bad93e54304fa6d5
Aurélien BESOMBES <oreo81150@gmail.com>
2025-12-16 10:16:47 +0100

    16/12/2025-4

--- monitoring/setup-monitoring.sh
+++ monitoring/setup-monitoring.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 set -e
 
+# Mettre à jour et installer les dépendances de base sauf curl
 yum update -y
-yum install -y git docker
+yum install -y git docker || true
 
-curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
-  -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
+# Docker
 systemctl enable docker
 systemctl start docker
 
-# Attendre que Docker soit prêt
-sleep 10
+# Docker Compose (sans yum)
+if ! command -v docker-compose &> /dev/null
+then
+  curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64" \
+    -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+fi
 
-git clone https://github.com/Oreo81/aws-project-scaling.git /opt/aws-project-scaling
+# Node Exporter
+useradd --no-create-home --shell /sbin/nologin node_exporter || true
+NODE_EXPORTER_VERSION="1.8.2"
+curl -LO "https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz"
+tar xvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+cp node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+chmod 755 /usr/local/bin/node_exporter
 
-chown -R ec2-user:ec2-user /opt/aws-project-scaling
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Prometheus Node Exporter
+After=network.target
 
-# FIX PERMISSIONS GRAFANA
-mkdir -p /opt/aws-project-scaling/monitoring/grafana/data
-chown -R 472:472 /opt/aws-project-scaling/monitoring/grafana/data
-chmod -R 775 /opt/aws-project-scaling/monitoring/grafana/data
+[Service]
+User=node_exporter
+Group=node_exporter
+ExecStart=/usr/local/bin/node_exporter
 
-cd /opt/aws-project-scaling/monitoring
-docker-compose up -d
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable node_exporter
+systemctl start node_exporter
